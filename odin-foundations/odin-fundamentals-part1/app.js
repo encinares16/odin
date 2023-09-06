0da071fc0a782489c8fa2707aa0f5d6a2f2eeb91
Jerome Encinares Talavera <139388162+encinares16@users.noreply.github.com>
2023-09-06 20:44:30 +0800

    upload app.js
    odin-assignment : fountdations-fundamental-1

---
+++ odin-foundations/odin-fundamentals-part1/app.js
@@ -0,0 +1,52 @@
+// Fundamentals Part 1 - Assignment
+
+// 1. 
+console.log(23 + 97)
+
+// 2.
+console.log(1 + 2 + 3 + 7 + 8 + 9)
+
+// 3. 
+console.log((4 + 6 + 9) / 77)
+
+// 4. 
+let a = 10
+console.log(a)
+console.log(9 * a)
+let b = 7 * a
+console.log(b)
+
+// 5.
+const max = 57
+actual = max - 13
+percentage = actual / max
+console.log(percentage)
+
+// 6.
+let myNumber = 5
+
+console.log(myNumber = 5) //  ->  5   
+console.log(myNumber == 5) // ->  true 
+console.log(myNumber === 5) // ->  true 
+
+console.log(myNumber == '5') // ->  true 
+console.log(myNumber === '5') // ->  false 
+
+let newNumber = '5'
+console.log(newNumber === '5') // ->  true 
+
+// 7.
+console.log(result = 10 * '5') // ->  50
+console.log(result = 10 / '5') // ->  2
+console.log(result = 10 - '5') // ->  2
+
+let five = '5'
+console.log(result = 10 + five) // ->  105 concatenate
+console.log(typeof five) // -> five is a string
+console.log(result = 10 + Number(five)) // ->  15
+
+console.log(typeof Number(five))  // -> five is now a number
+
+
+
+
